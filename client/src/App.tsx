b3cfa532dff9b3bce12d800c4b55af917db4dc1f
HurasAdam <huradam@wp.pl>
2024-03-19 17:01:55 +0100

    Feat: Added React Router Dom Navigation, SugestedPost, BreadCrumbs and ArticleDetailPage components.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home/Home"
 import "./App.css"
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 
 
 function App() {
@@ -7,7 +10,11 @@ function App() {
 
   return (
     <div className="App font-opensans">
-      <Home />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route index path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
+
     </div>
   )
 }
