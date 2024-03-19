03b6ea083abc1a713900548f05fc3d1115f9e986
HurasAdam <huradam@wp.pl>
2024-03-19 01:44:59 +0100

    Fix: Fixed Header responsivness for mobilne devices.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,40 +3,38 @@ import { images } from '../constants'
 import { MdKeyboardArrowDown, MdOutlineMenu } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import { headerLinks } from '../constants'
+import { INavItemProps } from '../types';
 
 
 
 
 
-enum ItemType {
-    Link = 'link',
-    Dropdown = 'dropdown',
-}
 
-type INavItemProps = {
-    item: {
-        name: string;
-        type: ItemType | string;
-        items?: string[];
-    }
-}
 
 
 const NavItem: React.FC<INavItemProps> = ({ item }) => {
+    const [dropdown, setDrop] = useState<boolean>(false)
+
+    const toggleDropdownHandler = (): void => {
+        setDrop((prevState) => !prevState)
+    }
+
     return (
         <li className=' relative group'>
-            {item.type === "link" ? <>
+            {item.type === "link" ? (<>
                 <a href="/" className='px-4 py-2'>{item.name}</a>
-                <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span>
-            </> : <>
-                <a href="/" className='px-4 py-2 flex gap-x-1 items-center'>
+                <span className=' cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>/</span>
+            </>) : (<div className='flex flex-col items-center'>
+                <button className='px-4 py-2 flex gap-x-1 items-center' onClick={toggleDropdownHandler}>
                     <span>
                         {item.name}
                     </span>
                     <MdKeyboardArrowDown />
-                </a>
-                <div className='hidden transition-all duration-500 pt-4 absolute bottom-[-20] right-[-10] transform traslate-y-full group-hover:block w-max'>
-                    <ul className='flex flex-col shadow-lg rounded-lg overflow-hidden'>
+                </button>
+
+                <div className={`${dropdown ? "block" : "hidden"
+                    } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-[-85px] lg:right-[-10] lg:transform lg:traslate-y-full lg:group-hover:block w-max`}>
+                    <ul className='bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden'>
                         {item.items?.map((page) => {
                             return (
 
@@ -48,7 +46,7 @@ const NavItem: React.FC<INavItemProps> = ({ item }) => {
                         })}
                     </ul>
                 </div>
-            </>}
+            </div>)}
 
         </li>
     )
@@ -62,7 +60,7 @@ const Header: React.FC = () => {
     }
 
     return (
-        <section>
+        <section className='sticky top-0 left-0 right-0 z-50'>
             <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
                 <div>
                     <img className='w-16' src={images.Logo} alt="logo" />
