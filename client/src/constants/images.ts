fbe24df2d1acab448875c49d0607a7bf5b63a904
HurasAdam <huradam@wp.pl>
2024-03-19 04:14:28 +0100

    Feat: Add CTA component for newsletter subscription.

--- client/src/constants/images.ts
+++ client/src/constants/images.ts
@@ -2,12 +2,14 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import Post1 from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/posts/ctaImage.jpg"
 
 const images = {
     Logo,
     HeroImage,
     Post1,
-    PostProfileImage
+    PostProfileImage,
+    CtaImage,
 }
 
 export default images;
