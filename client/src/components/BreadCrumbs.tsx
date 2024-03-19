b974bcd4341017c02b78f595e92df51bb4f55ae5
HurasAdam <huradam@wp.pl>
2024-03-19 17:25:03 +0100

    Fix: Added keys to dynamically generated elements to resolve console warnings and errors.

--- client/src/components/BreadCrumbs.tsx
+++ client/src/components/BreadCrumbs.tsx
@@ -19,7 +19,7 @@ const BreadCrumbs: React.FC<BreadCrumbsProps> = ({ data }) => {
         >
             {data.map((item, index) => {
                 return (
-                    <div className='text-black opacity-50 text-xs font-roboto md:text-sm '>
+                    <div key={index} className='text-black opacity-50 text-xs font-roboto md:text-sm '>
                         <Link to={item.link}>{item.name}</Link>
                         {index !== data.length - 1 && <span className='px-3'>/</span>}
                     </div>
