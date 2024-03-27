8938447ce70ef09782f27b7165017b2f22ebd329
HurasAdam <huradam@wp.pl>
2024-03-27 17:01:06 +0100

    Refactor: Update Articles and ArticleCard components to display dynamic API data.

--- client/src/components/ArticleCard.tsx
+++ client/src/components/ArticleCard.tsx
@@ -1,48 +1,56 @@
 import React from 'react'
 import { images } from "../constants"
 import { BsCheckLg } from "react-icons/bs";
+import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 
-const ArticleCard: React.FC = ({ className }) => {
+const ArticleCard: React.FC = ({ post, className }) => {
     return (
         <div
             className={`rounded-xl overflow-hidden 
         shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}
         
         `}>
-            <img
-                src={images.Post1}
-                alt="title"
-                className='w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60' />
+            <Link to={`/blog/${post._id}`}>
+                <img
+                    src={post.photo ? post.photo : images.Post3}
+                    alt="title"
+                    className='w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60' />
+            </Link>
 
             <div className='p-5'>
-                <h2
-                    className='font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px] '>
-                    Future of Work
-                </h2>
+                <Link to={`/blog/${post._id}`}>
+                    <h2
+                        className='font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px] '>
+                        {post.title}
+                    </h2>
+                </Link>
                 <p className='text-dark-light mt-3 text-sm md:text-lg'>
-                    Majority of people will work in jobs that don't exist today.
+                    {post.caption}
                 </p>
                 <div className='flex justify-between flex-nowrap items-center mt-6'>
 
                     <div className='flex items-center gap-x-2 md:gap-x-2.5'>
                         <img
-                            src={images.PostProfileImage}
+                            src={post.user.avatar ? post.user.avatar : images.userImage}
                             alt="post profile"
-                            className='w-9 h-9 md:w-10 md:h-10' />
+                            className='w-9 h-9 md:w-10 md:h-10 rounded-full' />
                         <div className='flex flex-col'>
                             <h4 className='font-bold italic text-dark-soft text-sm md:text-base '>
-                                Andrew Jones
+                                {post.user.name}
                             </h4>
                             <div className='flex items-center gap-x-2'>
                                 <span
-                                    className='bg-[#36B37E] w-fit bg-opacity-20 p-1.5 rounded-full'>
-                                    <BsCheckLg
-                                        className='w-1.5 h-1.5 text-[#36B37E]' />
+                                    className={`${post.user.verified ? "bg-[#36B37E]" : "bg-red-600"} w-fit bg-opacity-20 p-1.5 rounded-full`}>
+                                    {post.user.verified ? (<BsCheckLg
+                                        className='w-2 h-2 text-[#36B37E]' />) :
+                                        <AiOutlineClose className='w-2 h-2 text-red-500' />
+                                    }
                                 </span>
                                 <span
                                     className='italic text-dark-light text-xs md:text-sm'>
-                                    Verified writer
+                                    {post.user.verified ? "Verified " : "Unverified"} writer
                                 </span>
                             </div>
                         </div>
@@ -50,7 +58,21 @@ const ArticleCard: React.FC = ({ className }) => {
 
                     <span
                         className='font-bold text-dark-light italic text-sm md:text-base'>
-                        02 May
+                        {new Date(post.createdAt).getDate()}{" "}
+                        {new Date(post.createdAt).toLocaleString("en-US", {
+
+                            month: "long",
+
+
+                        })}
+                        <p className='text-xs text-slate-400 text-right  '>
+                            {new Date(post.createdAt).toLocaleString("en-US", {
+
+                                year: "numeric",
+
+
+                            })}
+                        </p>
                     </span>
 
 
--- client/src/pages/home/container/Articles.tsx
+++ client/src/pages/home/container/Articles.tsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import ArticleCard from '../../../components/ArticleCard'
 import { FaArrowRight } from 'react-icons/fa'
+import { useQuery } from '@tanstack/react-query'
+import { getAllPosts } from '../../../services/postApi'
+import toast from 'react-hot-toast'
+import ArticleCardSkeleton from '../../../components/comments/ArticleCardSkeleton'
+import ErrorMessage from '../../../components/comments/ErrorMessage'
 
 const Articles: React.FC = () => {
 
+    const { data: posts, isLoading, isError } = useQuery({
+        queryFn: () => getAllPosts(),
+        queryKey: ["posts"],
+        onError: (error: Error) => {
+            toast.error(error.message);
+        }
+    })
     return (
         <section
-            className='flex flex-col container mx-auto px-5 py-10'>
+            className='flex flex-col container mx-auto px-5 py-10 '>
 
             <div className='flex flex-wrap md:gap-x-5 gap-y-5 pb-10'>
+                {isLoading ? (
 
-                <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-                <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
-                <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+                    [...Array(6)].map((item, index) => {
+                        return (
+                            <ArticleCardSkeleton
+                                key={index}
+                                className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]"
+                            />
+                        )
+                    })
+                ) : isError ? <ErrorMessage message="Something went wrong..." /> : (posts.map((post) => {
+                    return (
+                        <ArticleCard
+                            key={post._id}
+                            post={post}
+                            className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)]" />
+                    )
+                }))}
+
+
 
             </div>
             <button
