a2681793f14b77a7ef2cf99a73590e588af82ca3
HurasAdam <huradam@wp.pl>
2024-03-27 04:53:44 +0100

    Refactor: rework Comment model schema.

--- backend/models/Comment.ts
+++ backend/models/Comment.ts
@@ -4,12 +4,12 @@ import * as types from "../shared/types";
 const CommentSchema = new Schema<types.IComment>({
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     description: { type: String, required: true },
-    postId: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
     check: { type: Boolean, default: false },
     parent: { type: Schema.Types.ObjectId, ref: "Comment", default: null },
     replyOnUser: { type: Schema.Types.ObjectId, ref: "User", default: null }
 
-}, { timestamps: true })
+}, { timestamps: true, toJSON: { virtuals: true } })
 
 CommentSchema.virtual('replies', {
     ref: "Comment",
